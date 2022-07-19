4543ed5b7f7141960dbe9189b92eac37f259e6e1
Aryamaan Srivastava <63562112+Aryamaan777@users.noreply.github.com>
2022-07-19 20:14:04 +0530

    Update AssociateSkills.java

--- Aryamaan_SkillTracker-Java-master/SkillTracker-Java-master/src/main/java/com/tracker/model/AssociateSkills.java
+++ Aryamaan_SkillTracker-Java-master/SkillTracker-Java-master/src/main/java/com/tracker/model/AssociateSkills.java
@@ -2,19 +2,12 @@ package com.tracker.model;
 
 import java.io.Serializable;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class AssociateSkills.
- */
 public class AssociateSkills implements Serializable{
 
-    /** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
 
-	/** The associate id. */
 	int associateId;
 	
-	/** The skill id. */
 	int skillId;
 	
 	
